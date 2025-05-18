205781711d6fa5ed5191304b99f1ea0da8f56d01
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-05-18 20:22:56 +0900

    script 수정

--- script/loginjs.js
+++ script/loginjs.js
@@ -9,8 +9,7 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
   const isValidBirthFormat = /^\d{4}-\d{2}-\d{2}$/.test(birth);
   const isRealDate = !isNaN(date.getTime());
   const isPast = date <= today;
-  const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-
+  
   // 생년월일 검사
   if (!isValidBirthFormat || !isRealDate || !isPast) {
     alert("올바른 생년월일을 입력해주세요 (예: 2000-01-01)");
